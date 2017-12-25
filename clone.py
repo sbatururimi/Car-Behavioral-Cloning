c2b69b1e2f79544d134a85fa4a8709bc6afc8a6e
Stas Batururimi <blueocean87@me.com>
2017-12-25 12:56:57 +0300

    flipping images & measurements for data augmentation

--- clone.py
+++ clone.py
@@ -16,20 +16,32 @@ for line in lines:
 	current_path = '../data-car-behavioral/IMG/' + filename
 	image = cv2.imread(current_path)
 	images.append(image)
-
+	# fleeping images & steering measurements
 	measurement = float(line[3])
 	measurements.append(measurement)
 
-X_train = np.array(images)
-# print(X_train.shape)
+# X_train = np.array(images)
+# y_train = np.array(measurements)
 
-y_train = np.array(measurements)
+# data augmentation
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+
+	augmented_images.append(cv2.flip(image, 1))
+	augmented_measurements.append(measurement * -1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# LeNet
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Convolution2D(6, 5, 5, activation='relu'))
